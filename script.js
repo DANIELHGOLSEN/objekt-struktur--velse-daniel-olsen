006cef87150ebd3550ef5a04ac77e5a9355436d9
daniel <danielolsen030619@gmail.com>
2026-09-03 10:58:41 +0200

    Opgave 2 færdig

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const favoritMon = {
-    vandtype: "Carracosta",
-    normaltype: "Cinccino",
-    jordtype: "Excadrill",
-    stentype: "Carracosta",
-    graestype: "Torterra",
-    moerktype: "Weavile",
-    staaltype: "Excadrill",
+    vandType: "Carracosta",
+    normalType: "Cinccino",
+    jordType: "Excadrill",
+    stenType: "Carracosta",
+    graesType: "Torterra",
+    moerkType: "Weavile",
+    staalType: "Excadrill",
+    topTrae: ["Carracosta", "Dragalge", "Excadrill"],
+    bedstegens: {
+        nummerEt: 5,
+        nummerTo: 3,
+        nummerTrae: 4,
+    },
 }
 
-console.log(favoritMon.vandtype);
-console.log(favoritMon["normaltype"]);
-console.log(favoritMon.jordtype);
-console.log(favoritMon["stentype"]);
+// Opgave 1
+// console.log(favoritMon.vandType);
+// console.log(favoritMon["normalType"]);
+// console.log(favoritMon.jordType);
+// console.log(favoritMon["stenType"]);
 
+// Opgave 2
+favoritMon.topTrae.forEach(function (topTrae) {
+    console.log(topTrae);
+})
 
-
+console.log(favoritMon.bedstegens.nummerTrae);
