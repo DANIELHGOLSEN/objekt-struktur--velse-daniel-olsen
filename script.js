952eed75577ca6dd7175a10ff1bc582ba3f309a3
daniel <danielolsen030619@gmail.com>
2026-09-03 10:47:08 +0200

    favoritMon er blevet logget, opgave 1.

---
+++ script.js
@@ -0,0 +1,17 @@
+const favoritMon = {
+    vandtype: "Carracosta",
+    normaltype: "Cinccino",
+    jordtype: "Excadrill",
+    stentype: "Carracosta",
+    graestype: "Torterra",
+    moerktype: "Weavile",
+    staaltype: "Excadrill",
+}
+
+console.log(favoritMon.vandtype);
+console.log(favoritMon["normaltype"]);
+console.log(favoritMon.jordtype);
+console.log(favoritMon["stentype"]);
+
+
+
